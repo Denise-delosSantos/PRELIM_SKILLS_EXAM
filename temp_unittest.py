8ef8b1c9a3f7d40ef9cbf3d5acfab7c9282e1d42
Denise-delosSantos <89897732+Denise-delosSantos@users.noreply.github.com>
2021-10-09 01:43:34 +0000

    Update temp_unittest.py

--- temp_unittest.py
+++ temp_unittest.py
@@ -5,7 +5,7 @@ class kelvin_test(unittest.TestCase):
     def test_celsius(self):
         self.assertEqual(Temperature(celsius=3).kelvin,276.15)
     def test_fahrenheit(self):
-        self.assertEqual(Temperature(fahrenheit=2).kelvin,256.4833)
+        self.assertEqual(Temperature(fahrenheit=2).kelvin,256.4833333333333)
     def test_kelvin(self):
         self.assertEqual(Temperature(kelvin=-1).kelvin,0)
     def same_kelvin(self):
